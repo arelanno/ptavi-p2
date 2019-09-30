d7424f0ab839c4532969ed79cb87d81a202c00ae
arelanno <a.relano@alumnos.urjc.es>
2019-09-30 21:24:34 +0200

    correcciones de pycodestyle realizadas en calcoo.py

--- calcoo.py
+++ calcoo.py
@@ -3,6 +3,7 @@
 
 import sys
 
+
 class Calculadora():
 
     def sum(self, op1, op2):
@@ -10,6 +11,7 @@ class Calculadora():
 
     def rest(self, op1, op2):
         return op1 - op2
+
 
 if __name__ == '__main__':
     try:
